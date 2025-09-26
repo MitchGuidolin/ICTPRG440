f9c547cd2d7386f9f6277591c82d6c6bd8788ebb
MitchGuidolin <mitchell.guidolin@studytafensw.edu.au>
2025-09-26 10:59:46 +0930

    TASK 3

---
+++ main.py
@@ -0,0 +1,98 @@
+""" scripts to view and reproject spatial vector data """
+
+#import the necessary libraries
+
+import geopandas as gpd  #gpd is an alias for GeoPandas and will be used to shorten the code and make it more efficeient to use the library.
+
+"""""Global Perameters"""
+
+FILE_PATH = r"C:\Programming\Spatial_data_original\Fire_and_Rescue.shp"  #Define input shapefile path
+
+OUTPUT_PATH = r"C:\Programming\Output" #Defined output folder 
+
+"""This fuction reads the vector data from a specified path and returns a Geodataframe"""
+
+# def read_return_gdf(FILE_PATH):
+
+#     gdf = gpd.read_file(FILE_PATH) 
+
+#     return gdf
+
+# gdf = read_return_gdf(FILE_PATH)
+
+# print(gdf) #validation of dataframe
+
+
+
+"""This function is to show spatial data and attriutes row by row"""
+
+
+
+# def print_all_rows(FILE_PATH):
+#     try:
+#         gdf = gpd.read_file(FILE_PATH)
+#         print(f"File loaded successfully.")
+      
+#     except Exception as e:
+#         print(f"Error reading file: {e}")
+#         return
+
+#     if gdf.empty:
+#         print("GeoDataFrame is empty — no rows to print.")
+#         return
+
+#     for row in gdf.iterrows():
+        
+#         print(row)
+#         print("-" * 60)
+
+
+# print_all_rows(FILE_PATH)
+
+
+
+# gdf = gpd.read_file(FILE_PATH)
+
+# gdf = gdf.to_crs(epsg=7856)
+
+# print(gdf.crs)
+# print(gdf.head())
+
+def transform_From_Projected_To_Geographic(FILE_PATH,EPSG=7856):
+
+    """this function reads a shapefile from a selected file path to a geodata frame, then convert coordinate system to an EPSG of choice, then save tranformed gdf as a shapefile to output folder"""
+
+    try:
+        gdf = gpd.read_file(FILE_PATH)
+        gdf = gdf.to_crs(epsg=EPSG)  #transormed from projected crs to geographic
+    except Exception as e:
+        print("Error",e)
+
+    return gdf #returns geodata frame object
+
+  
+
+"""export gdf to GeoJson"""
+
+def export_to_GeoJson(gdf,OUTPUT_PATH):
+
+    """this function take a geodataframe as a perameter gdf
+    it takes output folder as string perameter
+    returns none"""
+
+
+    full_output_path_with_filename = OUTPUT_PATH + "\\" + "output.geojson"
+    print(full_output_path_with_filename) #validate correct file name and path 
+
+    gdf.to_file(full_output_path_with_filename, driver="GeoJSON")
+
+    return None
+
+# # print(Transformgdf)  #validate transformation
+# # print(Transformgdf.crs)  #validate transformation
+Transformgdf = transform_From_Projected_To_Geographic(FILE_PATH,EPSG=7844) #by adding ,ESPG= after FILE_PATH we can overwrite the default value, carrying out a new crs tranformation, making the script easily reusable.
+export_to_GeoJson(Transformgdf,OUTPUT_PATH)
+
+
+
+
